9f407689b26677370b3853f2e2dd3447f30d3627
Godwin Duliente <rualgodwin@gmail.com>
2023-06-13 08:58:29 -0700

    added announcements and navbar

---
+++ views/_announcements.php
@@ -0,0 +1,31 @@
+<div class="announcements">
+ <div class="page-intro">
+  <div class="group">
+   <span>Latest</span>
+   <div class="hr-line"></div>
+   <i class="fa-solid fa-bullhorn"></i>
+  </div>
+  <span>Announcement</span>
+ </div>
+ <div class="announcement-details">
+  <span class="title">Title Placeholder</span>
+  <p class="description">Vestibulum nunc ante, ultricies at eleifend nec, dictum in ante.</p>
+  <div class="group">
+   <div class="announcer">
+    <img src="" alt="">
+    <span class="posted-by">Announced by John Doe</span>
+   </div>
+   <span class="timestamp">7d ago</span>
+  </div>
+ </div>
+ <div class="article">
+   <div class="sample-img"></div>
+   <p class="content">
+    Donec in neque sed lorem facilisis rhoncus. Morbi egestas diam sed massa vulputate, a malesuada eros lobortis. Vestibulum sed malesuada ipsum, dictum pretium ligula. Fusce sed dui at ante vehicula faucibus. 
+    <br><br>
+    Vestibulum urna ante, tempor non ligula nec, vestibulum congue ligula. Donec venenatis leo vestibulum aliquam suscipit. Donec auctor sapien et nisl pellentesque iaculis. Mauris porta in turpis et malesuada. Maecenas tempus, lacus nec bibendum rhoncus, lacus lectus ornare ante, ac elementum ligula odio id lorem. Cras at finibus massa, sed bibendum est. Aliquam a nisi eu eros tempus pharetra. Donec nisi lectus, vestibulum in elit blandit, varius commodo lorem. Integer sollicitudin fringilla fringilla. 
+    <br> <br>
+    Sed at magna hendrerit turpis egestas rhoncus ut at lectus. Vestibulum tincidunt ultrices mauris, vel euismod magna porta ut. Pellentesque at dolor porttitor, consectetur nisl et, accumsan mauris.
+   </p>
+  </div>
+</div>
