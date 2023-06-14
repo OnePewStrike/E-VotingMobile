1a0ef957281d8ec427f94db016326687d8bb4307
Godwin Duliente <rualgodwin@gmail.com>
2023-06-14 03:35:35 -0700

    added voting page

---
+++ views/_voting-page.php
@@ -0,0 +1,36 @@
+<div class="voting">
+ <div class="election-details">
+  <div class="title">[Election Name]</div>
+ </div>
+ <form class="modify" action="" method="">
+  <div class="position">
+   <span class="select">Select 1</span>
+   <span class="title">/President</span>
+  </div>
+  <div class="candidates-group">
+   <?php for($i=0; $i<=5;$i++) { ?>
+    <div class="candidate">
+     <div class="group">
+      <img src="" alt="">
+      <div class="candidate-details">
+       <div class="name">John Doe</div>
+       <div class="party-name">Party People</div>
+      </div>
+     </div>
+     <div class="field checkbox">
+      <input type="checkbox">
+     </div>
+    </div>
+   <?php } ?>
+  </div>
+  <div class="abstain">
+   <p class="description">I would like to abstain voting for this position.</p>
+   <div class="field checkbox">
+    <input type="checkbox">
+   </div>
+  </div>
+ </form>
+ <div class="actions">
+  <button class="primary wide">Next</button>
+ </div>
+</div>
