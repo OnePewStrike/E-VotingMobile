0087b736247028fe071a3112519a248b2cbb7586
Godwin Duliente <rualgodwin@gmail.com>
2023-06-14 02:19:52 -0700

    added voting-access denied

---
+++ views/_voting-access-deny.php
@@ -0,0 +1,9 @@
+<div class="voting-access">
+  <img src="assets/images/election_voting.svg" alt="Election Voting">
+  <div class="page-details">
+   <div class="title">[Custom Error Message]</div>
+  </div>
+  <div class="actions">
+   <button class="secondary">Return to Announcements</button>
+  </div>
+</div>
